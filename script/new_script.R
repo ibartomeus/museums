0e5e648db93abdacedaa186e88cf5219437468c6
Jamie Stavert <jsta096@UoA.auckland.ac.nz>
2018-03-20 13:15:08 +1300

    added NZ data graphs to the manuscript

--- script/new_script.R
+++ script/new_script.R
@@ -74,6 +74,7 @@ p
 
 
 library(reshape2)
+#need to give unique dataframe names to each group of taxa estimates
 xx2_melt <- melt(xx2)
 colnames(xx2_melt)[2] <- "estimate"
 se_melt <- melt(se)
@@ -81,17 +82,18 @@ colnames(se_melt)[2] <- "se"
 xx2_join <- merge(xx2_melt, se_melt, by="variable")
 time <- as.data.frame(time)
 xx <- cbind(xx2_join,time)
-m <- lm(estimate~time, data=xx)
-summary(m)
+p <- predict(m)
+xxx <- cbind(xx, p)
 
-predict(m)
-
+#need to plot each plot separately and combine plots at end (can't facet them)
 #plot chnages in fly richness across 10 time periods
-p <- ggplot(xx2_join, aes(time, estimate))
-p <- p + xlab(NULL) + ylab("Number of fly species")
+p <- ggplot(xxx, aes(variable, estimate, group=1))
+p <- p + xlab("Year") + ylab("Number of fly species")
+p <- p + scale_x_discrete(labels=c("1909-1924","1924-1940","1940-1949","1949-1962","1962-1974","1974-1977","1977-1980","1980-1986","1986-1996","1996-2007"))
 p <- p + theme(text = element_text(size=18))
 p <- p + geom_point(size = 3)
-p <- p + geom_abline(intercept = 24.90350, slope = -0.05035,linetype="dashed")#intercpt seems too high??
+p <- p + geom_abline(intercept = 4.5868425, slope = 0.139648, linetype="dashed")#intercept = first year*coef-intercept
+                                                                            #slope = coef *10
 p <- p + geom_errorbar(aes(ymin=estimate-se, ymax=estimate+se), width = 0)
 p <- p + theme(panel.grid.minor = element_blank(),
                panel.background = element_blank(),
@@ -108,4 +110,10 @@ p <- p + theme(legend.position="none")
 p <- p + theme(axis.title.y=element_text(margin=margin(0,20,0,0)))
 p
 
+par(mfrow = c(2,2), mar = c(4,3,1,1))
+
+plot(time,t(xx2), xlab = "", las = 2, ylab = "Number of species", main = "Exotic flies", xaxt = "n", ylim = c(min(xx2, na.rm = TRUE)- max(se), max(xx2, na.rm = TRUE)+ max(se)))
+axis(1, at = time ,labels = lab, las = 2, cex.axis = 0.7)
+arrows(time,as.numeric(xx2)+as.numeric(se), time, as.numeric(xx2)-as.numeric(se), angle=90, length = 0)
+abline(m, lty = 2)
 
